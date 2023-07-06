eafd9f25f5107e36b8b12ab59cdf932c47478f29
Jack Song <xujiesong1993@gmail.com>
2023-07-06 21:25:53 +1000

    Updates

---
+++ precache-manifest.029cd4dcdeacb9232169921f75de29a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f071671ac577c9f99c373bfb3806c84f",
+    "url": "/Lendi_RJS/index.html"
+  },
+  {
+    "revision": "91ea38d520040aaa36d7",
+    "url": "/Lendi_RJS/static/css/main.37085b45.chunk.css"
+  },
+  {
+    "revision": "2a8c5d21746c09017e36",
+    "url": "/Lendi_RJS/static/js/2.8c1e4ea9.chunk.js"
+  },
+  {
+    "revision": "91ea38d520040aaa36d7",
+    "url": "/Lendi_RJS/static/js/main.296ad139.chunk.js"
+  },
+  {
+    "revision": "88564888c0bc05a33b4b",
+    "url": "/Lendi_RJS/static/js/runtime~main.4f41ead8.js"
+  }
+]);
